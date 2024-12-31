fa4a139537a69cf0d64f6cce43e2e7c6e2569b85
Gabriel Souza <gabriel.sert9@gmail.com>
2024-12-31 11:06:20 +0100

    feat: created null check for Id class and tests

--- blog-backend/src/test/java/com/gabriel/blog/domain/valueobjects/IdTest.java
+++ blog-backend/src/test/java/com/gabriel/blog/domain/valueobjects/IdTest.java
@@ -4,13 +4,15 @@ import com.gabriel.blog.domain.exceptions.DomainException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class IdTest {
 
 	@Test
 	void shouldCreateCorrectId() {
-		assertThrows(DomainException.class, () -> new Id(null));
+		final var thrown = assertThrows(DomainException.class, () -> new Id(null));
+		assertEquals("Tried to create an id with a null value", thrown.getMessage());
 		assertDoesNotThrow(() -> new Id("any"));
 	}
 }
