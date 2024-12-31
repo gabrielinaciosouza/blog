98a8f24515387fb7e4654d8128f489d3ffe2d75d
Gabriel Souza <gabriel.sert9@gmail.com>
2024-12-31 11:06:20 +0100

    feat: created null check for Id class and tests

--- blog-backend/src/main/java/com/gabriel/blog/domain/valueobjects/Id.java
+++ blog-backend/src/main/java/com/gabriel/blog/domain/valueobjects/Id.java
@@ -5,6 +5,6 @@ import com.gabriel.blog.domain.Assertions;
 public record Id(String value) implements Assertions {
 
 	public Id {
-		nonNull(value, "Id cannot be null");
+		nonNull(value, "Tried to create an id with a null value");
 	}
 }
