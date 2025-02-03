286173c12af3bb618fffefd4064a3807dc7ebf3a
Gabriel <gabriel.sert9@gmail.com>
2025-02-03 11:06:13 +0100

    fix: try fix the build in gh

--- blog-backend/src/main/java/com/gabriel/blog/presentation/exceptions/GlobalExceptionHandler.java
+++ blog-backend/src/main/java/com/gabriel/blog/presentation/exceptions/GlobalExceptionHandler.java
@@ -9,6 +9,9 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import org.jboss.logging.Logger;
+
+import java.util.Arrays;
 
 /**
  * Global exception handler class for Quarkus.
@@ -16,6 +19,8 @@ import jakarta.ws.rs.ext.Provider;
  */
 @Provider
 public class GlobalExceptionHandler implements ExceptionMapper<Throwable> {
+
+  private final Logger logger = Logger.getLogger(GlobalExceptionHandler.class);
 
   /**
    * Handles custom and generic exceptions, returning appropriate responses.
@@ -26,23 +31,25 @@ public class GlobalExceptionHandler implements ExceptionMapper<Throwable> {
   @Override
   public Response toResponse(final Throwable exception) {
 
+    logger.error(exception.getMessage(), exception);
+
     return switch (exception) {
       case final RepositoryException repositoryException ->
           Response.status(Response.Status.INTERNAL_SERVER_ERROR)
               .type(MediaType.APPLICATION_JSON)
-              .entity(new ErrorResponse("Database error", repositoryException.getMessage()))
+              .entity(new ErrorResponse("Database error", repositoryException.getMessage(), Arrays.toString(exception.getStackTrace())))
               .build();
       case final DomainException domainException -> Response.status(422)
           .type(MediaType.APPLICATION_JSON)
-          .entity(new ErrorResponse("Domain error", domainException.getMessage()))
+          .entity(new ErrorResponse("Domain error", domainException.getMessage(), Arrays.toString(exception.getStackTrace())))
           .build();
       case final ValidationException validationException -> Response.status(400)
           .type(MediaType.APPLICATION_JSON)
-          .entity(new ErrorResponse("Validation error", validationException.getMessage()))
+          .entity(new ErrorResponse("Validation error", validationException.getMessage(), Arrays.toString(exception.getStackTrace())))
           .build();
       default -> Response.status(Response.Status.INTERNAL_SERVER_ERROR)
           .type(MediaType.APPLICATION_JSON)
-          .entity(new ErrorResponse("Unexpected error", exception.getMessage()))
+          .entity(new ErrorResponse("Unexpected error", exception.getMessage(), Arrays.toString(exception.getStackTrace())))
           .build();
     };
   }
@@ -50,10 +57,12 @@ public class GlobalExceptionHandler implements ExceptionMapper<Throwable> {
   private static class ErrorResponse {
     private String error;
     private String message;
+    private String stackTrace;
 
-    public ErrorResponse(final String error, final String message) {
+    public ErrorResponse(final String error, final String message, final String stackTrace) {
       this.error = error;
       this.message = message;
+      this.stackTrace = stackTrace;
     }
 
     public ErrorResponse() {
@@ -74,6 +83,15 @@ public class GlobalExceptionHandler implements ExceptionMapper<Throwable> {
     public void setMessage(final String message) {
       this.message = message;
     }
+
+    public String getStackTrace() {
+      return stackTrace;
+    }
+
+    public void setStackTrace(final String stackTrace) {
+      this.stackTrace = stackTrace;
+    }
+
 
     @Override
     public String toString() {
