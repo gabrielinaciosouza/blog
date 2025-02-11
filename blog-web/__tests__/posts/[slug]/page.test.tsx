df3b3b6f477d8d9947fe22c3fc3b9126d218db95
Gabriel <gabriel.sert9@gmail.com>
2025-02-11 13:14:13 +0100

    test: adding test to cover additional scenario

--- blog-web/__tests__/posts/[slug]/page.test.tsx
+++ blog-web/__tests__/posts/[slug]/page.test.tsx
@@ -1,43 +1,61 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import SinglePage from '@/app/posts/[slug]/page';
-import { getPostBySlug } from '@/services/postService';
+import {getPostBySlug} from '@/services/postService';
 
 jest.mock('@/services/postService');
 
 const mockedGetPostBySlug = getPostBySlug as jest.MockedFunction<typeof getPostBySlug>;
 
 describe('SinglePage', () => {
-  it('should render the post content when post is found', () => {
-    const post = {
-      postId: '1',
-      title: 'Test Title',
-      content: '<p>This is the content of the post.</p>',
-      creationDate: '2021-12-12T00:00:00Z',
-      slug: 'test-title',
-    };
+    it('should render the post content when post is found', async () => {
+        const post = {
+            postId: '1',
+            title: 'Test Title',
+            content: '<p>This is the content of the post.</p>',
+            creationDate: '2021-12-12T00:00:00Z',
+            slug: 'test-title',
+        };
 
-    mockedGetPostBySlug.mockResolvedValueOnce(post);
+        mockedGetPostBySlug.mockResolvedValueOnce(post);
 
-    const { container } = render(<SinglePage params={{ slug: 'test-title' }} />);
+        const {container} = render(await SinglePage({
+            params: {
+                slug: 'test-title',
+            }
+        }));
 
-    waitFor(() => {
-      expect(screen.getByText('Test Title')).toBeInTheDocument();
-      expect(screen.getByText('Gabriel Inacio')).toBeInTheDocument();
-      expect(screen.getByText('12/12/2021')).toBeInTheDocument();
-      const paragraph = container.querySelector('p');
-      expect(paragraph?.innerHTML).toBe('This is the content of the post.');
+        waitFor(() => {
+            expect(screen.getByText('Test Title')).toBeInTheDocument();
+            expect(screen.getByText('Gabriel Inacio')).toBeInTheDocument();
+            expect(screen.getByText('12/12/2021')).toBeInTheDocument();
+            const paragraph = container.querySelector('p');
+            expect(paragraph?.innerHTML).toBe('This is the content of the post.');
+        });
     });
-  });
 
-  it('should render error message when post is not found', () => {
-    mockedGetPostBySlug.mockRejectedValueOnce(new Error('Post not found'));
+    it('should render error message when post is not found', async () => {
+        mockedGetPostBySlug.mockRejectedValueOnce(new Error('Post not found'));
 
-    render(<SinglePage params={{ slug: 'non-existent-post' }} />);
+        render(await SinglePage({
+            params: {
+                slug: 'non-existent-post'
+            }
+        }));
 
-    waitFor(() => {
-      expect(screen.getByText('Post Not Found')).toBeInTheDocument();
-      expect(screen.getByText('Post not found')).toBeInTheDocument();
+
+        expect(screen.getByText('Post Not Found')).toBeInTheDocument();
     });
-  });
+
+    it('should render unknown error message when post is not found', async () => {
+        mockedGetPostBySlug.mockRejectedValueOnce("Any error");
+
+        render(await SinglePage({
+            params: {
+                slug: 'non-existent-post'
+            }
+        }));
+
+
+        expect(screen.getByText("Unknown error")).toBeInTheDocument();
+    });
 });
--- blog-web/src/services/postService.ts
+++ blog-web/src/services/postService.ts
@@ -1,7 +1,7 @@
 import CreatePostRequest from "@/models/create-post-request";
 import Post from "@/models/post";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/posts";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8080/posts";
 
 export const createPost = async (request: CreatePostRequest): Promise<Post> => {
     const response = await fetch(API_URL, {
@@ -22,7 +22,7 @@ export const createPost = async (request: CreatePostRequest): Promise<Post> => {
 };
 
 export const getPostBySlug = async (slug: string): Promise<Post> => {
-    const response = await fetch(`http://localhost:8080/posts/${slug}`, {
+    const response = await fetch(`${API_URL}/${slug}`, {
         cache: "force-cache",
     });
 
