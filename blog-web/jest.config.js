48ab51410ce19a88fd8f96a6ad20c97532e8a755
Gabriel <gabriel.sert9@gmail.com>
2025-02-11 10:16:25 +0100

    chore: coverage setup

--- blog-web/jest.config.js
+++ blog-web/jest.config.js
@@ -13,9 +13,22 @@ const customJestConfig = {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
   testEnvironment: 'jest-environment-jsdom',
+  coverageProvider: 'v8',
+  collectCoverage: true,
   transformIgnorePatterns: [
     '/node_modules/(?!react-quill-new|quill|lodash-es|parchment)',
   ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    }
+  }
 };
 
 module.exports = async () => ({
