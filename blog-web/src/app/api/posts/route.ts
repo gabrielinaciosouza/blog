cae836315a6089ed3a1880a61635938efceadc43
Gabriel Souza <gabriel.sert9@gmail.com>
2025-02-15 10:24:27 +0100

    tests: added tests to route.ts

--- blog-web/src/app/api/posts/route.ts
+++ blog-web/src/app/api/posts/route.ts
@@ -2,11 +2,11 @@ import {NextRequest, NextResponse} from "next/server";
 import {getPosts} from "@/services/postService";
 
 export const GET = async (req: NextRequest) => {
-
+    
     try {
         const { searchParams } = new URL(req.url);
         const page = searchParams.get("page");
-        const pageSize = searchParams.get("size");
+        const pageSize = searchParams.get("size"); 
 
         const posts = await getPosts(Number.parseInt(page!), Number.parseInt(pageSize!));
 
