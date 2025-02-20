3dc4b6bdd90d70046d98abf654e3a38e75924c9e
Gabriel Souza <gabriel.sert9@gmail.com>
2025-02-20 08:39:41 +0100

    feat: enhance RandomUuidGenerator to handle null domain; add unit tests for ID generation

--- blog-backend/src/main/java/com/gabriel/blog/infrastructure/services/RandomUuidGenerator.java
+++ blog-backend/src/main/java/com/gabriel/blog/infrastructure/services/RandomUuidGenerator.java
@@ -3,14 +3,21 @@ package com.gabriel.blog.infrastructure.services;
 import com.gabriel.blog.application.qualifiers.RandomGenerator;
 import com.gabriel.blog.application.services.IdGenerator;
 import jakarta.enterprise.context.ApplicationScoped;
-
 import java.util.UUID;
 
+/**
+ * Random UUID generator.
+ *
+ * <p>Created by Gabriel Inacio de Souza on February 9, 2025.</p>
+ */
 @ApplicationScoped
 @RandomGenerator
 public class RandomUuidGenerator implements IdGenerator {
-	@Override
-	public String generateId(String domain) {
-		return UUID.randomUUID() + "-" + domain;
-	}
+  @Override
+  public String generateId(final String domain) {
+    if (domain == null) {
+      return UUID.randomUUID().toString();
+    }
+    return UUID.randomUUID() + "-" + domain;
+  }
 }
