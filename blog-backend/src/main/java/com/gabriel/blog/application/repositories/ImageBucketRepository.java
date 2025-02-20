3b6925d0b820a067d223034ff012264399c2aebe
Gabriel Souza <gabriel.sert9@gmail.com>
2025-02-20 20:26:41 +0100

    doc: add detailed Javadoc comments for ImageBucketRepository and its components

--- blog-backend/src/main/java/com/gabriel/blog/application/repositories/ImageBucketRepository.java
+++ blog-backend/src/main/java/com/gabriel/blog/application/repositories/ImageBucketRepository.java
@@ -2,48 +2,65 @@ package com.gabriel.blog.application.repositories;
 
 import com.gabriel.blog.domain.valueobjects.Image;
 
+/**
+ * The {@link ImageBucketRepository} interface represents the repository for managing image storage buckets.
+ *
+ * <p>This interface contains the methods for creating images in the storage bucket based on the specified
+ * image data and metadata.</p>
+ *
+ * <p>Created by Gabriel Inacio de Souza on February 2, 2025.</p>
+ */
 public interface ImageBucketRepository {
 
-	/**
-	 * Creates an image in the bucket based on the specified image data and metadata.
-	 *
-	 * @param params the image data and metadata for creating the image.
-	 * @return the created image as an {@link Image}.
-	 */
-	Image createImage(UploadImageParams params);
+  /**
+   * Creates an image in the bucket based on the specified image data and metadata.
+   *
+   * @param params the image data and metadata for creating the image.
+   * @return the created image as an {@link Image}.
+   */
+  Image createImage(UploadImageParams params);
 
-	/**
-	 * The {@link BucketType} enum represents the different types of image storage buckets in the system.
-	 *
-	 * <p>This enum contains the different types of image storage buckets that can be used to store images
-	 * in the system. Each bucket type has a unique name that identifies the bucket in the system.</p>
-	 *
-	 * <p>Created by Gabriel Inacio de Souza on February 2, 2025.</p>
-	 */
-	enum BucketType {
-		COVER_IMAGES("cover-images"),
-		CONTENT_IMAGES("content-images");
+  /**
+   * The {@link BucketType} enum represents the different types of image storage buckets in the system.
+   *
+   * <p>This enum contains the different types of image storage buckets that can be used to store images
+   * in the system. Each bucket type has a unique name that identifies the bucket in the system.</p>
+   *
+   * <p>Created by Gabriel Inacio de Souza on February 2, 2025.</p>
+   */
+  enum BucketType {
+    COVER_IMAGES("cover-images"),
+    CONTENT_IMAGES("content-images");
 
-		private final String bucketName;
+    private final String bucketName;
 
-		BucketType(final String bucketName) {
-			this.bucketName = bucketName;
-		}
+    BucketType(final String bucketName) {
+      this.bucketName = bucketName;
+    }
 
-		public static boolean existsType(final String bucketName) {
-			for (final BucketType type : BucketType.values()) {
-				if (type.getBucketName().equals(bucketName)) {
-					return true;
-				}
-			}
-			return false;
-		}
+    public static boolean existsType(final String bucketName) {
+      for (final BucketType type : BucketType.values()) {
+        if (type.getBucketName().equals(bucketName)) {
+          return true;
+        }
+      }
+      return false;
+    }
 
-		public String getBucketName() {
-			return bucketName;
-		}
-	}
+    public String getBucketName() {
+      return bucketName;
+    }
+  }
 
-	record UploadImageParams(byte[] fileData, String fileName, String fileMimeType, String bucketName) {
-	}
+  /**
+   * The {@link UploadImageParams} record represents the parameters required to upload an image to a storage bucket.
+   *
+   * <p>This record contains the image data, file name, file MIME type, and bucket name required to upload an image
+   * to the storage bucket.</p>
+   *
+   * <p>Created by Gabriel Inacio de Souza on February 2, 2025.</p>
+   */
+  record UploadImageParams(byte[] fileData, String fileName, String fileMimeType,
+                           String bucketName) {
+  }
 }
