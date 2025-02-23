26de0facb963254a4bf413bdd6fa84fa659e7f54
Gabriel Souza <gabriel.sert9@gmail.com>
2025-02-23 09:14:54 +0100

    test: update AuthLinks tests to reflect changes in link names and remove usePublishPost tests

--- blog-web/__tests__/components/authLinks/AuthLinks.test.tsx
+++ blog-web/__tests__/components/authLinks/AuthLinks.test.tsx
@@ -9,10 +9,10 @@ describe('AuthLinks', () => {
         expect(screen.getAllByText('Login')[0]).toBeInTheDocument();
     });
 
-    it('should render write and logout links when status is authenticated', () => {
+    it('should render admin and logout links when status is authenticated', () => {
         render(<AuthLinks authStatus='authenticated' />);
 
-        expect(screen.getByText('Write')).toBeInTheDocument();
+        expect(screen.getByText('Admin')).toBeInTheDocument();
         expect(screen.getByText('Logout')).toBeInTheDocument();
     });
 
@@ -38,13 +38,13 @@ describe('AuthLinks', () => {
 
     });
 
-    it('should render write and logout links in responsive menu when status is authenticated', () => {
+    it('should render admin and logout links in responsive menu when status is authenticated', () => {
         render(<AuthLinks authStatus="authenticated" />);
 
         const burgerIcon = screen.getByRole('button');
         fireEvent.click(burgerIcon);
 
-        expect(screen.getAllByText('Write')[0]).toBeInTheDocument();
+        expect(screen.getAllByText('Admin')[0]).toBeInTheDocument();
         expect(screen.getAllByText('Logout')[0]).toBeInTheDocument();
     });
 });
