70080c9d666c81e864623f569ab1710f5fd76acf
Gabriel Souza <gabriel.sert9@gmail.com>
2025-02-25 05:58:57 +0100

    feat: enhance admin page with pagination and loading state for posts

--- blog-web/src/app/admin/page.tsx
+++ blog-web/src/app/admin/page.tsx
@@ -1,23 +1,61 @@
 "use client"
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './admin.module.css';
 import PostTile from '@/components/postTile/PostTile';
 import Post from '@/models/post';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import Button from '@/components/button/Button';
+import useLoading from '@/hooks/useLoading';
+import Loading from '@/components/loading/Loading';
+import Pagination from '@/components/pagination/Pagination';
 
 const AdminPage = () => {
     const router = useRouter();
-    const [posts, setPosts] = useState<Post[]>([
-        new Post("1", 'Post 1', 'post-1', '2021-09-01', 'This is the content of post 1', 'coverImage.jpg'),
-        new Post("2", 'Post 2', 'post-2', '2021-09-02', 'This is the content of post 2', 'coverImage.jpg'),
-    ]);
+    const searchParams = useSearchParams();
+    const { isLoading, startLoading, stopLoading } = useLoading();
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [deletedPosts, setDeletedPosts] = useState<Post[]>([]);
+    const [totalCount, setTotalCount] = useState(0);
+    const page = parseInt(searchParams.get('page') || '1');
+    const pageSize = 10;
 
-    const [deletedPosts, setDeletedPosts] = useState<Post[]>([
-        new Post("3", 'Post 3', 'post-3', '2021-09-03', 'This is the content of post 3', 'coverImage.jpg'),
-        new Post("4", 'Post 4', 'post-4', '2021-09-04', 'This is the content of post 4', 'coverImage.jpg'),
-    ]);
+    useEffect(() => {
+        const fetchData = async () => {
+            startLoading();
+            try {
+                const [deletedResponse, postsResponse] = await Promise.all([
+                    fetch('/api/posts/deleted'),
+                    fetch(`/api/posts?page=${page}&size=${pageSize}`)
+                ]);
+
+                let deletedData = [];
+                let availablePosts = [];
+                let totalCountResult = 0;
+                if (deletedResponse.ok) {
+                    deletedData = await deletedResponse.json();
+                }
+
+                if (postsResponse.ok) {
+                    const {posts, totalCount } = await postsResponse.json();
+                    availablePosts = posts;
+                    totalCountResult = totalCount;
+                }
+
+                console.log('posts', posts);
+
+                setDeletedPosts(deletedData);
+                setPosts(availablePosts);
+                setTotalCount(totalCountResult);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                stopLoading();
+            }
+        };
+
+        fetchData();
+    }, [page, startLoading, stopLoading]);
 
     const handleEdit = (id: string) => {
         // Handle edit logic here
@@ -48,31 +86,43 @@ const AdminPage = () => {
         router.push('/create-post');
     };
 
+    const totalPages = Math.ceil(totalCount / pageSize);
+    const hasPrev = page > 1;
+    const hasNext = page < totalPages;
+
     return (
         <div className={styles.adminContainer}>
             <div className={styles.buttonContainer}>
                 <Button className={styles.createButton} onClick={handleCreateNewPost}>Create New Post</Button>
             </div>
-            <div className={styles.deletedPostsHeader}>Deleted Posts</div>
-            {deletedPosts.map(post => (
-                <PostTile
-                    key={post.postId}
-                    post={post}
-                    onEdit={handleEdit}
-                    onRestore={handleRestore}
-                    onOpen={() => router.push(`/post/${post.slug}`)}
-                />
-            ))}
-            <div className={styles.activePosts}>Active Posts</div>
-            {posts.map(post => (
-                <PostTile
-                    key={post.postId}
-                    post={post}
-                    onEdit={handleEdit}
-                    onDelete={handleDelete}
-                    onOpen={() => router.push(`/post/${post.slug}`)}
-                />
-            ))}
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <>
+                    <div className={styles.deletedPostsHeader}>Deleted Posts</div>
+                    {deletedPosts.map(post => (
+                        <PostTile
+                            key={post.postId}
+                            post={post}
+                            onEdit={handleEdit}
+                            onRestore={handleRestore}
+                            onOpen={() => router.push(`/post/${post.slug}`)}
+                        />
+                    ))}
+                    <div className={styles.activePosts}>Active Posts</div>
+                    {posts.map(post => (
+                        <PostTile
+                            key={post.postId}
+                            post={post}
+                            onEdit={handleEdit}
+                            onDelete={handleDelete}
+                            onOpen={() => router.push(`/post/${post.slug}`)}
+                        />
+                    ))}
+                    {totalCount > pageSize &&
+                        <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />}
+                </>
+            )}
         </div>
     );
 };
