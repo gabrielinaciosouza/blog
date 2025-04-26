a841a6c8752b0e69aa9fdffcbc9df82f08c61a4b
Gabriel Souza <gabriel.sert9@gmail.com>
2025-04-26 12:21:32 +0200

    feat: Introduce AnimatedText component and integrate it into Navbar; add tests for AnimatedText component

--- blog-web/src/components/navbar/Navbar.tsx
+++ blog-web/src/components/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import AnimatedImage from "@/components/animatedImage/AnimatedImage";
 import Link from "next/link";
 import AuthLinks from "@/components/authLinks/AuthLinks";
 import Divider from "@/components/divider/Divider";
+import AnimatedText from "@/components/animatedText/AnimatedText";
 
 interface NavbarProps {
     authStatus?: "authenticated" | "notauthenticated";
@@ -15,12 +16,12 @@ const Navbar: React.FC<NavbarProps> = ({ authStatus = "authenticated" }) => {
             <nav className={styles.container} aria-label="Main Navigation" role="navigation">
                 <div className={styles.logo}>
                     <AnimatedImage src="/logo2.png" alt="logo2" width={48} height={48} className={styles.image} />
-                    <span className={styles.textLogo}>GABRIEL INACIO</span>
+                    <span className={styles.textLogo}><AnimatedText>GABRIEL INACIO</AnimatedText></span>
                 </div>
                 <ul className={styles.links}>
-                    <li><Link href="/" className={styles.link}>Homepage</Link></li>
-                    <li><Link href="/" className={styles.link}>Contact</Link></li>
-                    <li><Link href="/" className={styles.link}>About</Link></li>
+                    <li><Link href="/" className={styles.link}><AnimatedText>Homepage</AnimatedText></Link></li>
+                    <li><Link href="/" className={styles.link}><AnimatedText>Contact</AnimatedText></Link></li>
+                    <li><Link href="/" className={styles.link}><AnimatedText>About</AnimatedText></Link></li>
                     <li><AuthLinks authStatus={authStatus} /></li>
                 </ul>
             </nav>
