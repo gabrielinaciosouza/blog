7a70e08d1da124e7e586aa80c31f519466573ac8
Gabriel Souza <gabriel.sert9@gmail.com>
2025-07-13 14:56:11 +0200

    feat: update application properties for local, test, and development profiles and rename GoogleCredentialsMockProducer to GoogleCredentialsConfig

--- blog-backend/src/main/java/com/gabriel/blog/infrastructure/config/GoogleCredentialsConfig.java
+++ blog-backend/src/main/java/com/gabriel/blog/infrastructure/config/GoogleCredentialsConfig.java
@@ -5,19 +5,17 @@ import com.google.cloud.NoCredentials;
 import io.quarkus.arc.profile.IfBuildProfile;
 import io.quarkus.test.Mock;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Default;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Singleton;
 
-@Mock
 @ApplicationScoped
-@IfBuildProfile(anyOf = {"dev", "test"})
-public class GoogleCredentialsMockProducer {
+public class GoogleCredentialsConfig {
 
+  @Mock
   @Produces
   @Singleton
-  @Default
-  public Credentials googleCredential() {
+  @IfBuildProfile(anyOf = { "test", "local" })
+  public Credentials localCredendials() {
     return NoCredentials.getInstance();
   }
 }
