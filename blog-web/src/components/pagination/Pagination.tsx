7f8cd77f9c9a808f62796dd0043228e749761e04
Gabriel Souza <gabriel.sert9@gmail.com>
2025-07-20 22:42:45 +0200

    feat: refactor Pagination component to use Tailwind CSS and remove unused styles

--- blog-web/src/components/pagination/Pagination.tsx
+++ blog-web/src/components/pagination/Pagination.tsx
@@ -1,24 +1,25 @@
 "use client";
 
 import React from "react";
-import styles from "./pagination.module.css";
 import { useRouter } from "next/navigation";
-import Button from "../button/Button";
+import { Button } from "@/components/ui/button";
 
-const Pagination = ({ page, hasPrev, hasNext } : {page: number, hasPrev: boolean, hasNext: boolean}) => {
+const Pagination = ({ page, hasPrev, hasNext }: { page: number, hasPrev: boolean, hasNext: boolean }) => {
     const router = useRouter();
 
     return (
-        <div className={styles.container}>
+        <div className="flex gap-4 justify-center items-center py-4">
             <Button
+                variant="outline"
                 disabled={!hasPrev}
-                onClick={() => router.push(`?page=${+page - +1}`)}
+                onClick={() => router.push(`?page=${page - 1}`)}
             >
                 Previous
             </Button>
             <Button
+                variant="outline"
                 disabled={!hasNext}
-                onClick={() => router.push(`?page=${+page + +1}`)}
+                onClick={() => router.push(`?page=${page + 1}`)}
             >
                 Next
             </Button>
