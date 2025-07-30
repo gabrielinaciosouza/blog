3f5ba0e39b9720ec1db6eaacc031e72aa494d85a
Gabriel Souza <gabriel.sert9@gmail.com>
2025-07-30 12:16:22 +0200

    fix problem with GetAccountInfo

--- blog-backend/src/main/java/com/gabriel/blog/infrastructure/config/GoogleCredentialsConfig.java
+++ blog-backend/src/main/java/com/gabriel/blog/infrastructure/config/GoogleCredentialsConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.Credentials;
 import com.google.cloud.NoCredentials;
 import com.google.cloud.logging.Severity;
 import com.google.common.util.concurrent.AbstractFuture;
+import com.google.firebase.auth.internal.GetAccountInfoResponse;
 import io.quarkus.arc.profile.IfBuildProfile;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.quarkus.test.Mock;
@@ -12,14 +13,14 @@ import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Singleton;
 
 @ApplicationScoped
-@RegisterForReflection(targets = {AbstractFuture.class, Severity.class})
+@RegisterForReflection(targets = {AbstractFuture.class, Severity.class, GetAccountInfoResponse.class})
 public class GoogleCredentialsConfig {
 
-  @Mock
-  @Produces
-  @Singleton
-  @IfBuildProfile(anyOf = { "test", "local" })
-  public Credentials localCredendials() {
-    return NoCredentials.getInstance();
-  }
+	@Mock
+	@Produces
+	@Singleton
+	@IfBuildProfile(anyOf = {"test", "local"})
+	public Credentials localCredendials() {
+		return NoCredentials.getInstance();
+	}
 }
